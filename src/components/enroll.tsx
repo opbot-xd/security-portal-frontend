552c33f05cbed39657ebc5e13391a7467068657c
Krishna Awasthi <awasthikrishna23052005@gmail.com>
2024-10-08 11:15:56 +0530

    feat:made login and request page according to figma design

--- src/components/enroll.tsx
+++ src/components/enroll.tsx
@@ -1,60 +1,105 @@
-import { useNavigate } from 'react-router-dom';
-import Drawer from '@/components/ui/Drawer';
+import React, { useState } from 'react';
+import { Box, Typography, Button, MenuItem, Select, FormControl, InputLabel, List, ListItem, Avatar, TextField } from '@mui/material';
 
-// Define the common Tailwind CSS class directly
-const commonBoxClasses = 'flex text-xl font-sans justify-center items-center h-40 w-80 bg-zinc-950 rounded-xl text-white cursor-pointer';
+const Dashboard: React.FC = () => {
+  const [requestType, setRequestType] = useState<string>('');
 
-type RouteMapKey = 'Student' | 'Faculty' | 'Temporary Faculty' | 'Faculty Family members';
-
-const EnrollPage: React.FC = () => {
-  const navigate = useNavigate();
-
-  const handleNavigation = (text: RouteMapKey) => {
-    // Mapping text to specific routes
-    const routeMap: Record<RouteMapKey, string> = {
-      Student: 'student',
-      Faculty: 'faculty',
-      'Temporary Faculty': 'temporary-faculty',
-      'Faculty Family members': 'faculty-family'
-    };
-    
-    // Navigate to the route based on the text
-    navigate(`/enroll/${routeMap[text]}`);
+  const handleRequestTypeChange = (event: any) => {
+    setRequestType(event.target.value);
   };
 
   return (
-    <div className="flex flex-col gap-40 bg-slate-900 justify-center items-center h-screen">
-      <Drawer />
-      <div className="flex gap-40">
-        <div
-          onClick={() => handleNavigation('Student')}
-          className={commonBoxClasses}
-        >
-          Student
-        </div>
-        <div
-          onClick={() => handleNavigation('Faculty')}
-          className={commonBoxClasses}
-        >
-          Faculty
-        </div>
-      </div>
-      <div className="flex gap-40">
-        <div
-          onClick={() => handleNavigation('Temporary Faculty')}
-          className={commonBoxClasses}
-        >
-          Temporary Faculty
-        </div>
-        <div
-          onClick={() => handleNavigation('Faculty Family members')}
-          className={commonBoxClasses}
-        >
-          Faculty Family members
-        </div>
-      </div>
-    </div>
-  );
-}
+    <Box sx={{ display: 'flex', height: '100vh', backgroundColor: '#f4f4f9' }}>
+      {/* Sidebar */}
+      <Box sx={{ width: 250, backgroundColor: '#fff', padding: '20px', borderRight: '1px solid #ccc', display: "flex", flexDirection: "column"}}>
+        <Typography variant="h6" sx={{ marginBottom: '30px', color: '#2980B9', fontSize: '28px', fontFamily:"Roboto", fontWeight: "800"   }}>Security App</Typography>
+        <List component="nav" className='mt-40'>
+          <ListItem button sx={{ backgroundColor: '#1976d2', color: '#fff', marginBottom: '10px' }}>
+            Raise Request
+          </ListItem>
+          <ListItem button sx={{fontWeight: "600", marginBottom: '10px'}}>Request History</ListItem>
+          <ListItem button sx={{fontWeight: "600"}}>Settings</ListItem>
+        </List>
+        <Box className="bg-[url('logo.svg')] bg-left-top bg-cover h-20 w-48 justify-self-end">
+          
+        </Box>
+      </Box>
 
-export default EnrollPage;
+      {/* Main Content */}
+      <Box sx={{ flexGrow: 1, padding: '20px' }}>
+        <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+          <Typography variant="h4">Raise Request</Typography>
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Typography variant="body1" sx={{ marginRight: '10px' }}>LOGGED IN AS MDGSPACE</Typography>
+            <Avatar>M</Avatar>
+          </Box>
+        </Box>
+
+        <Box sx={{ marginTop: '20px' }}>
+          <FormControl fullWidth>
+            <InputLabel>Select Request Type</InputLabel>
+            <Select
+              value={requestType}
+              onChange={handleRequestTypeChange}
+              label="Select Request Type"
+            >
+              <MenuItem value="gate_pass">Gate Pass</MenuItem>
+              <MenuItem value="material_pass">Material Pass</MenuItem>
+              <MenuItem value="labour_pass">Labour Pass</MenuItem>
+              <MenuItem value="new_faculty">New Faculty</MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
+        {requestType === 'gate_pass' && (
+          <Box sx={{ marginTop: '20px'}}>
+            <form>
+              <Box sx={{ display: 'flex', justifyContent: 'space-between', marginBottom: '20px' }}>
+                <Box sx={{ width: '48%' }}>
+                  <Typography variant="body1" sx={{ marginBottom: '5px' }}>Issuer Id</Typography>
+                  <TextField label="Issuer Id" fullWidth variant="outlined" />
+                </Box>
+                <Box sx={{ width: '48%' }}>
+                  <Typography variant="body1" sx={{ marginBottom: '5px' }}>Issuer Name</Typography>
+                  <TextField label="Issuer Name" fullWidth variant="outlined" />
+                </Box>
+              </Box>
+              <Box sx={{ display: 'flex', justifyContent: 'start',gap:"55px", marginBottom: '20px' }}>
+                <Box sx={{ width: '25%' }}>
+                  <Typography variant="body1" sx={{ marginBottom: '5px' }}>Accomplice</Typography>
+                  <TextField fullWidth label="Accomplice" variant="outlined" />
+                </Box>
+                <Box sx={{ width: '25%' }}>
+                  <Typography variant="body1" sx={{ marginBottom: '5px' }}>Relation</Typography>
+                  <TextField fullWidth label="Relation" variant="outlined" />
+                </Box>
+                <Box sx={{ width: '25%' }}>
+                  <Typography variant="body1" sx={{ marginBottom: '5px' }}>Purpose</Typography>
+                  <TextField fullWidth label="Purpose" variant="outlined" />
+                </Box>
+              </Box>
+              <Box sx={{ display: 'flex', justifyContent: 'space-between', marginBottom: '20px' }}>
+                
+              </Box>
+              <Box sx={{ display: 'flex', justifyContent: 'space-between', marginBottom: '20px' }}>
+                <Box sx={{ width: '48%' }}>
+                  <Typography variant="body1" sx={{ marginBottom: '5px' }}>Arrival Date</Typography>
+                  <TextField fullWidth variant="outlined" />
+                </Box>
+                <Box sx={{ width: '48%' }}>
+                  <Typography variant="body1" sx={{ marginBottom: '5px' }}>Departure Date</Typography>
+                  <TextField fullWidth variant="outlined" />
+                </Box>
+              </Box>
+              <div className='flex justify-center items-center'><Button variant="contained" color="primary" type="submit" sx={{justifySelf:"center"}}>
+                Submit
+              </Button></div>
+              
+            </form>
+          </Box>
+        )}
+      </Box>
+    </Box>
+  );
+};
+
+export default Dashboard;
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,102 +1,70 @@
-import React from "react";
-import { cn } from "@/lib/utils";
-import { Label } from "@/components/ui/Label";
-import { Input } from "@/components/ui/input";
-import { basicAxiosPost } from '@/services/basic-axios';
+import { useState, ChangeEvent } from 'react';
+import { TextField, Button, MenuItem, Typography, Link, Box } from '@mui/material';
+
+  
 
 
 export function LoginForm() {
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");  
+  const [loginType, setLoginType] = useState<string>('');
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const creds = { username: email, password: password };
-  
-    try {
-      const responseData = await basicAxiosPost(
-        '', // IMPORT endpoint
-        creds // data to be sent in POST request
-      );
-  
-  
-      if (responseData.status === 200) {
-        sessionStorage.setItem(
-          'username',
-          responseData.data.data['user_data']['username']
-        );
-        sessionStorage.setItem(
-          'token',
-          JSON.stringify(responseData.data.data['token'])
-        );
-  
-        window.location.href = '/enroll';
-      }
-    } catch (error) {
-      console.error('Login failed', error);
-    }
+  const handleLoginTypeChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    setLoginType(event.target.value);
   };
-
-  const onChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
-  };
-  const onChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value);
-  };
-  return (
-    <div className="flex bg-slate-900 justify-center items-center h-screen">
-      <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
-        <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
-          Security Admin Portal
-        </h2>
-        <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
-          Portal to handle all the admin tasks related to Security
-        </p>
-
-        <form className="my-8" onSubmit={handleSubmit}>
-          <LabelInputContainer className="mb-4">
-            <Label htmlFor='text'>Username</Label>
-            <Input id="text" placeholder="admin" type="text" onChange={onChangeEmail} />
-          </LabelInputContainer>
-          <LabelInputContainer className="mb-4">
-            <Label htmlFor="password">Password</Label>
-            <Input id="password" placeholder="••••••••" type="password" onChange={onChangePassword}/>
-          </LabelInputContainer>
-
-          <button
-            className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
-            type="submit"
-          >
-            Sign up &rarr;
-            <BottomGradient />
-          </button>
-
-          <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
-        </form>
-      </div>
-    </div>
-  );
-}
-
-const BottomGradient = () => {
   return (
     <>
-      <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent" />
-      <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent" />
+    <div className="h-screen w-screen bg-[url('login2.svg')] bg-center bg-cover flex justify-end">
+      <div className="h-screen w-1/2 flex justify-center items-center">
+      <Box
+      sx={{
+        width: 400,
+        margin: 'auto',
+        padding: '2rem',
+        borderRadius: '8px',
+        boxShadow: 3,
+        backgroundColor: '#fff',
+        textAlign: 'center',
+      }}
+    >
+      <Typography variant="h5" gutterBottom>
+        Welcome to Security App
+      </Typography>
+      <TextField
+        select
+        label="Login Type"
+        value={loginType}
+        onChange={handleLoginTypeChange}
+        fullWidth
+        margin="normal"
+      >
+        <MenuItem value="admin">Admin</MenuItem>
+        <MenuItem value="user">User</MenuItem>
+        <MenuItem value="guest">Guest</MenuItem>
+      </TextField>
+      <TextField
+        label="Login ID"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+      />
+      <TextField
+        label="Password"
+        variant="outlined"
+        type="password"
+        fullWidth
+        margin="normal"
+      />
+      <Link href="#" variant="body2" sx={{ display: 'block', margin: '10px 0' }}>
+        Forgot Password?
+      </Link>
+      <Button variant="contained" color="primary" fullWidth>
+        Login
+      </Button>
+      <Link href="#" variant="body2" sx={{ display: 'block', marginTop: '10px' }}>
+        Register new user
+      </Link>
+    </Box>
+      </div>
+    </div>
     </>
   );
-};
-
-const LabelInputContainer = ({
-  children,
-  className,
-}: {
-  children: React.ReactNode;
-  className?: string;
-}) => {
-  return (
-    <div className={cn("flex flex-col space-y-2 w-full", className)}>
-      {children}
-    </div>
-  );
-};
+}
